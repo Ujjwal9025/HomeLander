c86a5ea8c485dd914925a22627890ad4beb6f811
Ujjwal Kumar <92972515+Ujjwal9025@users.noreply.github.com>
2025-01-13 22:58:10 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://homelander-7wkw.onrender.com',
         secure: false,
       },
     },
