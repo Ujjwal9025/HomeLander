41770a278c401851cfaa4bdf3d2752ebd9dbab07
ujjwal-up <ujjwal@upliance.ai>
2025-01-14 00:25:46 +0530

    update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  authDomain: "mern-estate-7be0d.firebaseapp.com",
+  projectId: "mern-estate-7be0d",
+  storageBucket: "mern-estate-7be0d.appspot.com",
+  messagingSenderId: "130845141616",
+  appId: "1:130845141616:web:98bda2ebd6a2ba523ba2b5"
 };
 
 // Initialize Firebase
