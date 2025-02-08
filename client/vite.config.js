12286ab994a54dd42ee8a55bcf7ba9e7bded225f
ujjwal-up <ujjwal@upliance.ai>
2025-02-08 21:41:38 +0530

    env for backend route

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) // Use import.meta.url instead of process.cwd()
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_ENDPOINT,
+          secure: false,
+        },
       },
     },
-  },
-
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
